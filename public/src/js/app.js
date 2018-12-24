3732c5cbfdb8ac259e50b28a10723690e4d3c98c
Kirill255 <jhgbnm1@mail.ru>
2018-12-24 21:46:05 +0300

    registering a service worker

---
+++ public/src/js/app.js
@@ -0,0 +1,12 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function(reg) {
+      // регистрация сработала
+      console.log("Registration succeeded. Scope is " + reg.scope);
+    })
+    .catch(function(error) {
+      // регистрация прошла неудачно
+      console.log("Registration failed with " + error);
+    });
+}
