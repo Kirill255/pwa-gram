5a411e4e802bd47c1f19ce542d4eec915135e57c
Kirill255 <jhgbnm1@mail.ru>
2018-12-25 17:26:35 +0300

    cache multiple files with addAll

--- public/sw.js
+++ public/sw.js
@@ -6,9 +6,21 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("static").then(cache => {
       console.log("The service worker is precaching app shell");
-      cache.add("/"); // добавляем файл в кэш
-      cache.add("/index.html"); // добавляем файл в кэш
-      cache.add("/src/js/app.js"); // добавляем файл в кэш
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/promise.js",
+        "/src/js/fetch.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+      ]); // добавляем файлы в кэш
     })
   );
 });
