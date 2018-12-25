cf95e78cacd781a03842ae7f7c7d4919f99c5b46
Kirill255 <jhgbnm1@mail.ru>
2018-12-26 00:35:17 +0300

    applying cache only

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,23 @@
 var CACHE_STATIC_NAME = "static-v5";
 var CACHE_DYNAMIC_NAME = "dynamic-v4";
 
+var STATIC_FILES = [
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/src/js/app.js",
+  "/src/js/feed.js",
+  "/src/js/promise.js",
+  "/src/js/fetch.js",
+  "/src/js/material.min.js",
+  "/src/css/app.css",
+  "/src/css/feed.css",
+  "/src/images/main-image.jpg",
+  "https://fonts.googleapis.com/css?family=Roboto:400,700",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+];
+
 self.addEventListener("install", e => {
   console.log("The service worker is being installed.", e);
   // caches.open(); // https://developer.mozilla.org/en-US/docs/Web/API/Cache
@@ -10,22 +27,7 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE_STATIC_NAME).then(cache => {
       console.log("The service worker is precaching app shell");
-      cache.addAll([
-        "/",
-        "/index.html",
-        "/offline.html",
-        "/src/js/app.js",
-        "/src/js/feed.js",
-        "/src/js/promise.js",
-        "/src/js/fetch.js",
-        "/src/js/material.min.js",
-        "/src/css/app.css",
-        "/src/css/feed.css",
-        "/src/images/main-image.jpg",
-        "https://fonts.googleapis.com/css?family=Roboto:400,700",
-        "https://fonts.googleapis.com/icon?family=Material+Icons",
-        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
-      ]); // добавляем файлы в кэш
+      cache.addAll(STATIC_FILES); // добавляем файлы в кэш
     })
   );
 });
@@ -95,6 +97,8 @@ self.addEventListener("fetch", e => {
         });
       })
     );
+  } else if (new RegExp("\\b" + STATIC_FILES.join("\\b") + "\\b").test(e.request.url)) {
+    e.respondWith(caches.match(e.request));
   } else {
     e.respondWith(
       caches.match(e.request).then(response => {
