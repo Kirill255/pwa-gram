fe4cebc2bc03f712a08b739d41f238759f2b07d4
Kirill255 <jhgbnm1@mail.ru>
2019-01-01 22:55:34 +0300

    sending push messages from the server

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const cors = require("cors")({ origin: true });
 
+var webpush = require("web-push");
+
 // https://console.firebase.google.com/project/pwa-gram-9114d/settings/serviceaccounts/adminsdk
 const serviceAccount = require("./pwa-gram-9114d-fb-key.json");
 admin.initializeApp({
@@ -26,6 +28,36 @@ exports.storePostData = functions.https.onRequest((request, response) => {
         image: request.body.image
       })
       .then(() => {
+        webpush.setVapidDetails(
+          "mailto:business@academind.com", // must be your a real valid email adress
+          "BMrfb7ujhKZE1zOPhgRNT2Ksmd24lQrDmVMa2e9pyfpA8uhclSF7kfaX_aopKJZgVCVKJharOu7gmkaIrNPMqq0", // Public Key
+          "QO4jEn44VqzqbO0rt0kPb7ct7tgyvdh5dUOef6DfBVg" // Private Key
+        );
+        return admin
+          .database()
+          .ref("subscriptions")
+          .once("value");
+      })
+      .then(subscriptions => {
+        subscriptions.forEach(sub => {
+          var pushConfig = {
+            endpoint: sub.val().endpoint,
+            keys: {
+              auth: sub.val().keys.auth,
+              p256dh: sub.val().keys.p256dh
+            }
+          };
+
+          webpush
+            .sendNotification(
+              pushConfig,
+              JSON.stringify({ title: "New Post", content: "New Post added!" }) // some payload
+            )
+            .catch(err => {
+              console.log(err);
+            });
+        });
+
         response.status(201).json({ message: "Data stored", id: request.body.id });
       })
       .catch(err => {
