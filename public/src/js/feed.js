f5ead44436694d9ef6942791b4bac1142337455f
Kirill255 <jhgbnm1@mail.ru>
2019-01-03 13:50:21 +0300

    getting DOM access

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -5,11 +5,19 @@ var sharedMomentsArea = document.querySelector("#shared-moments");
 var form = document.querySelector("#form");
 var inputTitle = document.querySelector("#title");
 var inputLocation = document.querySelector("#location");
+var videoPlayer = document.querySelector("#player");
+var canvas = document.querySelector("#canvas");
+var captureButton = document.querySelector("#capture-btn");
+var imagePicker = document.querySelector("#image-picker");
+var imagePickerArea = document.querySelector("#pick-image");
+
+function initializeMedia() {}
 
 function openCreatePostModal() {
   // createPostArea.style.display = "block";
   // setTimeout(function() {
   createPostArea.style.transform = "translateY(0)";
+  initializeMedia();
   // }, 1);
 
   if (deferredPrompt) {
