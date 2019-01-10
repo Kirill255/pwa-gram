b690641c9858e6331521c555db78fbc22a7f2369
Kirill255 <jhgbnm1@mail.ru>
2019-01-11 01:12:12 +0300

    changed formidable to busboy

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,13 @@ const cors = require("cors")({ origin: true });
 
 var webpush = require("web-push");
 
-var formidable = require("formidable");
+// var formidable = require("formidable");
+var Busboy = require("busboy");
 var UUID = require("uuid-v4");
 
 var fs = require("fs");
+var path = require("path");
+var os = require("os");
 
 var FIREBASE_PUBLIC_KEY =
   "BMrfb7ujhKZE1zOPhgRNT2Ksmd24lQrDmVMa2e9pyfpA8uhclSF7kfaX_aopKJZgVCVKJharOu7gmkaIrNPMqq0";
@@ -37,6 +40,125 @@ admin.initializeApp({
 exports.storePostData = functions.https.onRequest((request, response) => {
   // response.send("Hello from Firebase!");
   cors(request, response, () => {
+    /* модуль busboy */
+    var uuid = UUID();
+
+    const busboy = new Busboy({ headers: request.headers });
+    // These objects will store the values (file + fields) extracted from busboy
+    let upload;
+    const fields = {};
+
+    // This callback will be invoked for each file uploaded
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+      console.log(
+        `File [${fieldname}] filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`
+      );
+      const filepath = path.join(os.tmpdir(), filename);
+      upload = { file: filepath, type: mimetype };
+      file.pipe(fs.createWriteStream(filepath));
+    });
+
+    // This will invoked on every field detected
+    busboy.on("field", function(
+      fieldname,
+      val,
+      fieldnameTruncated,
+      valTruncated,
+      encoding,
+      mimetype
+    ) {
+      fields[fieldname] = val;
+    });
+
+    // This callback will be invoked after all uploaded files are saved.
+    busboy.on("finish", () => {
+      var bucket = gcs.bucket("pwagram-24f0c.appspot.com");
+      bucket.upload(
+        upload.file,
+        {
+          uploadType: "media",
+          metadata: {
+            metadata: {
+              contentType: upload.type,
+              firebaseStorageDownloadTokens: uuid
+            }
+          }
+        },
+        (err, file) => {
+          if (err) return new Error(err);
+
+          // ссылка на картинку в сторадже получится примерно такая https://firebasestorage.googleapis.com/v0/b/pwa-gram-9114d.appspot.com/o/car1.jpg?alt=media&token=201528cb-ae1b-4b9e-a248-b96fcc52a82f
+
+          admin
+            .database()
+            .ref("posts")
+            .push({
+              id: fields.id,
+              title: fields.title,
+              location: fields.location,
+              rawLocation: {
+                lat: fields.rawLocationLat,
+                lng: fields.rawLocationLng
+              },
+              image:
+                "https://firebasestorage.googleapis.com/v0/b/" +
+                bucket.name +
+                "/o/" +
+                encodeURIComponent(file.name) +
+                "?alt=media&token=" +
+                uuid
+            })
+            .then(() => {
+              webpush.setVapidDetails(
+                "mailto:jhgbnm1@mail.ru", // must be your a real valid email adress
+                FIREBASE_PUBLIC_KEY,
+                FIREBASE_PRIVATE_KEY
+              );
+              return admin
+                .database()
+                .ref("subscriptions")
+                .once("value");
+            })
+            .then(subscriptions => {
+              subscriptions.forEach(sub => {
+                var pushConfig = {
+                  endpoint: sub.val().endpoint,
+                  keys: {
+                    auth: sub.val().keys.auth,
+                    p256dh: sub.val().keys.p256dh
+                  }
+                };
+
+                webpush
+                  .sendNotification(
+                    pushConfig,
+                    JSON.stringify({
+                      title: "New Post",
+                      content: "New Post added!",
+                      openUrl: "/help"
+                    }) // some payload, openUrl can be any url address http://example.com
+                  )
+                  .catch(err => {
+                    console.log(err);
+                  });
+              });
+
+              response.status(201).json({ message: "Data stored", id: fields.id });
+            })
+            .catch(err => {
+              response.status(500).json({ error: err });
+            });
+        }
+      );
+    });
+
+    // The raw bytes of the upload will be in request.rawBody.  Send it to busboy, and get
+    // a callback when it's finished.
+    busboy.end(request.rawBody);
+    /*  */
+
+    /* модуль formidable, почему-то я не смог его запустить, доходит до момента `Service Worker is Syncing new Posts`, пост сохраняется в idb.js и не приходит уведомление, что пост отправлен в базу, идёт какая-то ошибка с кросдоменным запросом и как следствие firebase не запускает функцию на free плане, так как там ограничены запросы к сторонним ресурсам, в чём причина не разбирался! переписал на busboy */
+    /*
     var uuid = UUID();
 
     var formData = new formidable.IncomingForm();
@@ -81,7 +203,7 @@ exports.storePostData = functions.https.onRequest((request, response) => {
                 image:
                   "https://firebasestorage.googleapis.com/v0/b/" +
                   bucket.name +
-                  "/o" +
+                  "/o/" +
                   encodeURIComponent(file.name) +
                   "?alt=media&token=" +
                   uuid
@@ -130,5 +252,12 @@ exports.storePostData = functions.https.onRequest((request, response) => {
         );
       });
     });
+    */
   });
 });
+
+/*
+https://firebasestorage.googleapis.com/v0/b/pwagram-24f0c.appspot.com/o/2019-01-10T21%3A16%3A03.724Z.png?alt=media&token=51ea3cc1-2fdc-4a52-8ae5-970d7deec9c6
+
+https://firebasestorage.googleapis.com/v0/b/pwagram-24f0c.appspot.com/o2019-01-10T21%3A16%3A03.724Z.png?alt=media&token=51ea3cc1-2fdc-4a52-8ae5-970d7deec9c6
+*/
