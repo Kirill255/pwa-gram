0a07591a7eff6156342fcb7d8c84de1f557a080f
Kirill255 <nativemail@mail.ru>
2019-01-15 18:17:58 +0300

    configuring workbox precaching and generate new service-worker.js file

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  "globDirectory": "public/",
-  "globPatterns": [
-    "**/*.{html,ico,json,css,png,jpg,js}"
-  ],
-  "swDest": "public/service-worker.js"
+  globDirectory: "public/",
+  globPatterns: ["**/*.{html,ico,json,css,js}", "src/images/*.{png,jpg}"],
+  swDest: "public/service-worker.js",
+  globIgnores: ["help/**"]
 };
