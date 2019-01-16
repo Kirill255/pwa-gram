fc6aef87f2f5ef73ef82a6ee210ba1784249933d
Kirill255 <nativemail@mail.ru>
2019-01-16 16:11:20 +0300

    implementing routing with the workbox router and expanding dynamic caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,27 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.0.0-beta.0/workbox-sw.js");
 
+// в качестве роута может быть регулярка или просто строка
+workbox.routing.registerRoute(
+  /.*(?:googleapis|gstatic)\.com.*$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "google-fonts" // имя придумываем сами
+  })
+);
+
+workbox.routing.registerRoute(
+  "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "material-css"
+  })
+);
+
+workbox.routing.registerRoute(
+  /.*(?:firebasestorage\.googleapis)\.com.*$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "post-images"
+  })
+);
+
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute([
   {
@@ -64,7 +86,7 @@ workbox.precaching.precacheAndRoute([
   },
   {
     "url": "sw-base.js",
-    "revision": "122adf98ab62f4c9b15bfbf62ad2c3fd"
+    "revision": "cbe540cb74b18fe9beff227e5ce43621"
   },
   {
     "url": "sw.js",
--- public/sw-base.js
+++ public/sw-base.js
@@ -1,4 +1,31 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.0.0-beta.0/workbox-sw.js");
 
+// при генерации service-worker.js, workbox автоматически кеширует файлы находящиеся в public/ согласно конфигу, но также нам нужно закешировать и cdn-ресурсы(url), которые мы подключаем в index.html, также как мы это делали в sw.js
+
+// в качестве роута может быть регулярка или просто строка
+// шрифты
+workbox.routing.registerRoute(
+  /.*(?:googleapis|gstatic)\.com.*$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "google-fonts" // имя придумываем сами
+  })
+);
+
+// css lib
+workbox.routing.registerRoute(
+  "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "material-css"
+  })
+);
+
+// ссылки на изображения сохранённые в firebase storage
+workbox.routing.registerRoute(
+  /.*(?:firebasestorage\.googleapis)\.com.*$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "post-images"
+  })
+);
+
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute([], {});
